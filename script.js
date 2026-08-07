1a29e38e162eadde1cb6d17b307e335a044b3fcf
AleJesu1805 <alexanderj150823@gmail.com>
2026-08-07 07:30:54 -0400

    mejoras visuales

--- script.js
+++ script.js
@@ -2,6 +2,29 @@ const inputDolar = document.getElementById('dolar');
 const inputBolivar = document.getElementById('bolivar');
 const inputSol = document.getElementById('sol');
 
+const contenedorScroll = document.getElementById('contenedorScroll');
+
+function resetear(num) {
+    inputDolar.value = num;
+
+    let bolivarExacto = valorBolivar * inputDolar.value;
+    inputBolivar.value = bolivarExacto;
+
+    let solExacto = valorSol * inputDolar.value;
+    inputSol.value = solExacto;
+}
+
+for (let i = 1; i <= 10; i++) {
+    const btn = document.createElement("button");
+    btn.classList.add('atajo');
+    btn.id = `btn${i}`;
+    let orden = i * 5;
+    btn.setAttribute('onclick', `resetear(${orden})`);
+    btn.innerHTML = `${orden}$`;
+    contenedorScroll.appendChild(btn);
+}
+
+
 let valorBolivar = null;
 let valorSol = null;
 
@@ -32,7 +55,7 @@ inputDolar.addEventListener('input', (e) => {
 
     let solExacto = valorSol * inputDolar.value;
     inputSol.value = solExacto;
-})
+});
 
 inputBolivar.addEventListener('input', (e) => {
     let dolarExacto = inputBolivar.value / valorBolivar;
@@ -40,4 +63,12 @@ inputBolivar.addEventListener('input', (e) => {
 
     let solExacto = valorSol * inputDolar.value;
     inputSol.value = solExacto;
-})
+});
+
+inputSol.addEventListener('input', (e) => {
+    let dolarExacto = inputSol.value / valorSol;
+    inputDolar.value = dolarExacto;
+
+    let bolivarExacto = valorBolivar * inputDolar.value;
+    inputBolivar.value = bolivarExacto;
+});
